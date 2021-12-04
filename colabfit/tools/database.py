6effb61ba74a72b0f76ccdd6e43b6fc51f357ec4
Josh Vita <vita.joshua@gmail.com>
2021-12-04 11:55:56 -0600

    Checkpointing; need to handle previously-concat

--- colabfit/tools/database.py
+++ colabfit/tools/database.py
@@ -1,11 +1,11 @@
 import h5py
-import warnings
 import numpy as np
 
 from colabfit import (
     MAX_STRING_LENGTH,
     STRING_DTYPE_SPECIFIER
 )
+from colabfit.tools.configuration import Configuration
 
 
 class Database(h5py.File):
@@ -47,7 +47,7 @@ class Database(h5py.File):
                 :attr:`Configuration.info` dictionaries.
 
                 /info_field_1
-                    .attributes
+                    .attrs
                         concatenated
                             A boolean indicating if /data has been concatenated
                             or not
@@ -72,19 +72,37 @@ class Database(h5py.File):
                         will be a group of individual arrays indexed by their
                         corresponding configuration IDs
                     /slices
-                        A list Configuration IDs with the same shape as
-                        /data.shape[0], specifying which Configuration each row
-                        corresponds to.
+                        A group where /slices/config_id_1 specifies how to slice
+                        /data in order to get the data corresponding to
+                        config_id_1.
 
+                        Regardless of if 
+
+                        If /info_field_1.attrs['concatenated'] == False:
+                            
+                            Then /slices/config_id_1 will just be config_id_1.
+
+                        If /info_field_1.attrs['concatenated'] == True:
+
+                            Then /slices/config_id_1 will be an integer array.
+
+                        Regardless of if /data has been concatenated or not,
+                        the data for config_id_1 can be extracted using:
+
+                        ..code-block:: python
+
+                            group = database['configurations/info/info_field_1']
+                            data = group['data'][group['slices']]
+                
                         Some things to note:
 
                             * If the given info field CAN be concatenated as
-                            described in /data, then the IDs in /indices
+                            described in /data, then the IDs in /slices
                             will be duplicated based on how many rows each
                             Configuration contributes.
                             * If the given info field can't be concatenated as
                             described in /data, then /data will be a simple
-                            list of arrays, so /indices will just map an
+                            list of arrays, so /slices will just map an
                             entire array to a single Configuration.
                             * This use of lists of IDs is favorable because it
                             allows for quick filtering on /data and returning
@@ -106,7 +124,7 @@ class Database(h5py.File):
                 As in /configurations/ids, but for the Property IDs
 
             /property_1
-                .attributes
+                .attrs
                     definition
                         An OpenKIM Property Definition in dictionary format
 
@@ -159,7 +177,7 @@ class Database(h5py.File):
             /configuration_set_id_G
         /datasets
             /dataset_1
-                .attributes
+                .attrs
                     authors
                         A list of author names
                     links
@@ -244,10 +262,16 @@ class Database(h5py.File):
 
             for k, v in atoms.info.items():
                 g = self['configurations/info'].require_group(k)
+                # Extract data and slices groups
                 if 'data' in g:
-                    g = g['data']
+                    g_data = g['data']
                 else:
-                    g = g.create_group('data', track_order=True)
+                    g_data = g.create_group('data', track_order=True)
+
+                if 'slices' in g:
+                    g_slices = g['slices']
+                else:
+                    g_slices = g.create_group('slices', track_order=True)
 
                 if isinstance(v, str):
                     v = np.atleast_1d(v).astype(STRING_DTYPE_SPECIFIER)
@@ -258,7 +282,7 @@ class Database(h5py.File):
                     v = np.atleast_1d(v)
 
                 # Add in info fields
-                g.create_dataset(
+                g_data.create_dataset(
                     name=config_id,
                     shape=v.shape,
                     dtype=v.dtype,
@@ -266,21 +290,34 @@ class Database(h5py.File):
                     maxshape=(None,)+v.shape[1:],
                 )
 
+                g_slices[config_id] = config_id
+
+                g.attrs['concatenated'] = False
+
             for k, v in atoms.arrays.items():
                 g = self['configurations/arrays'].require_group(k)
                 if 'data' in g:
-                    g = g['data']
+                    g_data = g['data']
                 else:
-                    g = g.create_group('data', track_order=True)
+                    g_data = g.create_group('data', track_order=True)
+
+                if 'slices' in g:
+                    g_slices = g['slices']
+                else:
+                    g_slices = g.create_group('slices', track_order=True)
 
                 # Add in arrays fields
-                g.create_dataset(
+                g_data.create_dataset(
                     name=config_id,
                     shape=v.shape,
                     dtype=v.dtype,
                     data=v,  # should already be an array
                     maxshape=(None,)+v.shape[1:],
                 )
+
+                g_slices[config_id] = config_id
+
+                g.attrs['concatenated'] = False
 
         # IDs should always be concatenated
         self.concatenate_group(self['configurations/ids'])
@@ -301,9 +338,8 @@ class Database(h5py.File):
 
     def concatenate_group(self, group, chunks=None):
         """
-        This should attempt to concatenate all of the datasets in a group
-
-        TODO: decide if we need to preserve insertion order.
+        Attempt to concatenate all of the datasets in a group. Raise an
+        exception if the datasets in the group have incompatible shapes.
         """
 
         if isinstance(group, str):
@@ -322,11 +358,30 @@ class Database(h5py.File):
 
         start = 0
         problem_adding = False
+
+        # TODO: need to figure out how to handle the case where the field has
+        # already been concatenated before.
+
         for ds in group['data'].values():
             try:
-                data[start:start+ds.shape[0]] = ds
+                # Handle IDs differently? or give them slices
+
+                # Copy the data over
+                stop = start + ds.shape[0]
+                data[start:stop] = ds
+
+                if 'slices' in group:
+                    # Some groups (like IDs) don't need to be sliced
+
+                    if ds.name is not '_root_concatenated':
+                        # Ignore the one that's already been concatenated
+                        key = ds.name.split("/")[-1]
+
+                        del group[f'slices/{key}']
+                        group[f'slices/{key}'] = np.arange(start, stop)
+
                 start += ds.shape[0]
-            except:
+            except Exception as e:
                 problem_adding = True
                 break
 
@@ -341,7 +396,36 @@ class Database(h5py.File):
         else:
             # Get rid of all of the old stuff
             del group['data']
-            group['data'] = data
+            group['data']['_root_concatenated'] = data
+            group.attrs['concatenated'] = True
+
+
+    def get_configuration(self, i):
+        return self.get_configurations([i])
+
+    
+    def get_configurations(self, ids):
+        """
+        A generator that returns in-memory Configuration objects one at a time
+        by loading the info/array fields
+
+        Args:
+
+            ids (list):
+                A list of string IDs specifying which Configurations to return.
+        """
+
+        for co_id in ids:
+            co = Configuration()
+
+            g = self['configuration/info']
+            for field in g:
+                if g[field].attrs['concatenated']:
+                    # It's all one big dataset
+                    pass
+
+                co.info[field] = g[f'{field}/data'][()]
+                co.info.setdefault(field, )
 
 
     def parse_data(self):
