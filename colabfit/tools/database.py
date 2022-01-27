02c006416d4be39e93b63f9a3b915d02c809aae3
Josh Vita <vita.joshua@gmail.com>
2022-01-27 11:50:10 -0600

    Fix plotting... for real this time

--- colabfit/tools/database.py
+++ colabfit/tools/database.py
@@ -2063,7 +2063,7 @@ class MongoDatabase(MongoClient):
         nfields = len(fields)
 
         nrows = max(1, int(np.ceil(nfields/3)))
-        ncols = min(3, nfields%3)
+        ncols = 3 if nrows > 1 else min(3, nfields%3)
 
         if method == 'plotly':
             fig = make_subplots(rows=nrows, cols=ncols, subplot_titles=fields)
