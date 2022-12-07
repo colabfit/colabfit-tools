1a9da918e42ee0793fb558cb0d6f563a155d7bfb
efuem <ericfuemmeler@gmail.com>
2022-12-07 12:41:13 -0600

    Updated property defintions

--- colabfit/tools/property_definitions.py
+++ colabfit/tools/property_definitions.py
@@ -2,7 +2,7 @@ potential_energy_pd = {
     "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/potential-energy",
     "property-name": "potential-energy",
     "property-title": "Potential energy from a static calculation",
-    "property-description": "Potential energy from a calculation of a static configuration. Energies must be specified to be per-atom or supercell. If a reference energy has been used, this must be specified as well.",
+    "property-description": "Potential energy from a calculation of a static configuration. Energies may be specified to be per-atom or supercell. If a reference energy has been used, this must be specified as well.",
     "energy": {
         "type": "float",
         "has-unit": True,
@@ -14,8 +14,8 @@ potential_energy_pd = {
         "type": "bool",
         "has-unit": False,
         "extent": [],
-        "required": True,
-        "description": "If True, \"energy\" is the total energy of the system, and has NOT been divided by the number of atoms in the configuration."
+        "required": False,
+        "description": "If True, \"energy\" is the total energy of the system divided by the number of atoms in the configuration."
     },
     "reference-energy": {
         "type": "float",
