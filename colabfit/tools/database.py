d0c0de1d84973f377e083cac7fccdc9412863821
efuem <ericfuemmeler@gmail.com>
2023-03-22 16:20:19 -0500

    Updated update_configuration_set

--- colabfit/tools/database.py
+++ colabfit/tools/database.py
@@ -1699,7 +1699,7 @@ class MongoDatabase(MongoClient):
         # Get configuration ids from current version and append and/or remove
         #ids = cs_doc['relationships']['configurations']
         #ids = [i.split('_')[-1] for i in ids]
-        co_docs = list(self.configurations.find({'relationships.configuration_sets':cs_id}),{'hash': 1, '_id': 0})
+        co_docs = list(self.configurations.find({'relationships.configuration_sets':cs_id},{'hash': 1, '_id': 0}))
         ids = [i["hash"] for i in co_docs]
         init_len = len(ids)
 
