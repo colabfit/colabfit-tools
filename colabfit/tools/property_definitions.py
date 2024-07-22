041889ded068e9c3f52010c51d253ff64b60ec45
gpwolfe <g.patrick.wolfe@gmail.com>
2024-07-22 14:34:47 -0400

    Add band gap, formation, adsorption, atomization energy prop defs

--- colabfit/tools/property_definitions.py
+++ colabfit/tools/property_definitions.py
@@ -147,3 +147,118 @@ free_energy_pd = {
         'energy must have the same units as "energy".',
     },
 }
+
+{
+    "property-id": "tag:staff@noreply.colabfit.org,2022-11-18:property/atomization-energy",
+    "property-name": "atomization-energy",
+    "property-title": "Atomization energy from a static calculation",
+    "property-description": "Formation energy from a calculation of a static "
+    "configuration which uses atomic systems to compute reference energies. Energies "
+    "must be specified to be per-atom if true. If the reference energies are known, "
+    "these must be specified as well.",
+    "energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "The atomization energy of the system.",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": 'If True, "energy" is the atomization energy of the system '
+        "divided by the number of atoms in the configuration.",
+    },
+    "reference-energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "If known, represents values which have been subtracted from "
+        "the base energy, resulting in the atomization energy.",
+    },
+}
+adsorption_energy_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-11-18:property/adsorption-energy",
+    "property-name": "adsorption-energy",
+    "property-title": "Adsorption energy from a static calculation",
+    "property-description": "Adsorption energy from a calculation of a static "
+    "configuration which uses non-atomic systems to compute reference energies. "
+    "Energies must be specified to be per-atom if true. If the reference energies are "
+    "known, this must be specified as well.",
+    "energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "The adsorption energy of the system.",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": 'If True, "energy" is the adsorption energy of the system '
+        "divided by the number of atoms in the configuration.",
+    },
+    "reference-energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "If known, represents values which have been subtracted from "
+        "the base energy, resulting in the adsorption energy.",
+    },
+}
+
+
+formation_energy_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-11-18:property/formation-energy",
+    "property-name": "formation-energy",
+    "property-title": "Formation energy from a static calculation",
+    "property-description": "Formation energy from a calculation of a static "
+    "configuration which uses non-atomic systems to compute reference energies. "
+    "Energies must be specified to be per-atom if true. If the reference energies are "
+    "known, this must be specified as well.",
+    "energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "The formation energy of the system.",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": 'If True, "energy" is the formation energy of the system '
+        "divided by the number of atoms in the configuration.",
+    },
+    "reference-energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "If known, represents values which have been subtracted from "
+        "the base energy, resulting in the formation energy.",
+    },
+}
+
+
+band_gap_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-11-18:property/band-gap",
+    "property-name": "band-gap",
+    "property-title": "Band gap energy from a static calculation",
+    "property-description": "Band gap energy from a calculation of a static "
+    "configuration.",
+    "energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "The band gap energy of the system.",
+    },
+}
